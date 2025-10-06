165898c5827259d073478d32ae69f9943d6a8a50
Mohammad Saim <saim@Mohammads-MacBook-Pro.local>
2025-10-06 13:09:20 -0400

    add code

---
+++ sections/section3/test.py
@@ -0,0 +1,22 @@
+import unittest
+from code.my_calculations import Calculations
+
+class TestCalculations(unittest.TestCase):
+    def not_a_test_sum(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+    
+    def test_diff(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_difference(), 6, 'The difference is wrong.')
+    
+    def test_product(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_product(), 16, 'The product is wrong.')
+    
+    def test_quotient(self):
+        calculation = Calculations(8, 2)
+        self.assertEqual(calculation.get_quotient(), 4, 'The quotient is wrong.')
+
+if __name__ == '__main__':
+    unittest.main()
